61fff388f447e518769a9bf8f08a2679a7c56b73
Shun Sakai <sorairolake@protonmail.ch>
2021-11-14 07:58:56 +0900

    Improve the long version

--- src/macros.rs
+++ src/macros.rs
@@ -21,7 +21,7 @@ macro_rules! long_version {
         static LONG_VERSION: once_cell::sync::OnceCell<String> = once_cell::sync::OnceCell::new();
 
         LONG_VERSION.get_or_init(|| {
-            if let (Some(sha_short), Some(commit_date)) = (
+            let head = if let (Some(sha_short), Some(commit_date)) = (
                 option_env!("VERGEN_GIT_SHA_SHORT"),
                 option_env!("VERGEN_GIT_COMMIT_DATE"),
             ) {
@@ -33,7 +33,14 @@ macro_rules! long_version {
                 )
             } else {
                 env!("CARGO_PKG_VERSION").to_string()
-            }
+            };
+            format!(
+                "{}\n\n{}\n{}\n\n{}",
+                head,
+                "Copyright (C) 2021 Shun Sakai",
+                "License: GNU General Public License v3.0 or later",
+                "Report bugs to https://github.com/sorairolake/rshash/issues"
+            )
         })
     }};
 }
